5168916447bb70189005aeb5af09e21a022a35b7
Artemios Bespalko <artemios.bespalko@gmail.com>
2017-11-25 20:05:15 +0200

    Random speed rotation.

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -2,8 +2,29 @@
 
 public class Rotator : MonoBehaviour
 {
+    Quaternion originRotation;
+    float angle;
+    public float rotationSpeed;
+    float random;
+
+    public void Start()
+    {
+        originRotation = transform.rotation;
+        random = Random.Range(1 , 4);
+    }
+
+    private void FixedUpdate()
+    {
+        angle++;
+
+        Quaternion rotationY = Quaternion.AngleAxis(angle * random, Vector3.up);
+        Quaternion rotationX = Quaternion.AngleAxis(angle * random, Vector3.right);
+
+        transform.rotation = originRotation * rotationY * rotationX;
+    }
+
     public void Update()
     {
-        transform.Rotate(new Vector3(15f, 30f, 45f) * Time.deltaTime);
+      
     }
 }
